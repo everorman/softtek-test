b3937d291a081e94aa65f2f768c0abd298a24083
Evert Ortiz <evert.ortiz.m@gmail.com>
2024-05-08 12:10:33 -0500

    chore: mapping response

--- functions/search/handler.js
+++ functions/search/handler.js
@@ -1,15 +1,36 @@
 'use strict';
 const axios = require('axios');
+const dataMapping = (data) => {
+  const mapping = {
+    name: 'nombre',
+    gender: 'genero',
+    birth_year: 'fecha_nacimiento',
+    mass: 'peso'
+  };
+  const mappedData = {};
+
+  for (const key in mapping) {
+    if (data[key]) {
+      mappedData[mapping[key]] = data[key];
+    } else {
+      mappedData[key] = 'N/A';
+    }
+  }
+
+  return mappedData;
+};
 const search = async (event, context) => {
   const { search } = event.queryStringParameters;
-  console.log(`https://swapi.py4e.com/api/people/?search=${search}`);
   const { data, status } = await axios.get(`https://swapi.py4e.com/api/people/?search=${search}`);
+
   if (status !== 200) return {
     "statusCode": status
   };
+
+  const result = data.results.map(dataMapping);
   return {
     "statusCode": 200,
-    "body": JSON.stringify(data)
+    "body": JSON.stringify(result)
   };
 };
 
--- test/functions/search.test.js
+++ test/functions/search.test.js
@@ -1,5 +1,5 @@
 const { search } = require('../../functions/search/handler');
-
+const { PERSON_SEARCH } = require('../fixtures/personSearch');
 jest.mock('axios', () => ({
   get: jest.fn()
 }));
@@ -13,24 +13,23 @@ describe('search function', () => {
         search: 'Luke'
       }
     };
-
-    const mockData = {
-      name: 'Luke Skywalker',
-      height: '172',
-      mass: '77'
-    };
+    const expectedData = [{
+      nombre: PERSON_SEARCH.results[0].name,
+      genero: PERSON_SEARCH.results[0].gender,
+      fecha_nacimiento: PERSON_SEARCH.results[0].birth_year,
+      peso: PERSON_SEARCH.results[0].mass
+    }];
 
     const mockResponse = {
-      data: mockData,
+      data: PERSON_SEARCH,
       status: 200
     };
 
     axios.get.mockResolvedValue(mockResponse);
 
     const response = await search(event);
-
     expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual(JSON.stringify(mockData));
+    expect(response.body).toEqual(JSON.stringify(expectedData));
   });
 
   it('should return only status code when status is not 200', async () => {
